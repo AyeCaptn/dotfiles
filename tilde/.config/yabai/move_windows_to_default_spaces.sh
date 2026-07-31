802b157f0e04dcedc685beacdc2bfaf4427e35ca
Sem Uytterhoeven <sem.uytterhoeven@cr-38.be>
2026-07-31 10:59:51 +0200

    Fix the moving of windows

--- tilde/.config/yabai/move_windows_to_default_spaces.sh
+++ tilde/.config/yabai/move_windows_to_default_spaces.sh
@@ -5,7 +5,7 @@ while IFS=$(printf '\t') read -r id app; do
   case "$app" in
     Ghostty) space=terminal ;;
     Zen) space=browser ;;
-    Mail|"Microsoft Outlook"|WhatsApp|MSTeams|"Microsoft Teams") space=comms ;;
+    Mail|"Microsoft Outlook"|*WhatsApp*|MSTeams|"Microsoft Teams") space=comms ;;
     Obsidian|Notes) space=notes ;;
     Spotify) space=media ;;
     Calendar) space=calendar ;;
